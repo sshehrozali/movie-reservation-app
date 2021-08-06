7f8591578eb046bc4d1c3a9dd5f325dc1b1678b4
Shehroz Ali <sshehroza13@gmail.com>
2021-08-06 16:06:08 +0500

    Styled status bar for bookings and seat reservations

--- app/index/app.js
+++ app/index/app.js
@@ -1,22 +1,37 @@
 // Global Seat counter
 let seatCounter = 0;
+let remainingSeats = 0;
 
 // Add Seat
 function addSeat() {
-    seatCounter += 1;
+    seatCounter += 1;   // Update Seat Counter
+    remainings = 60 - seatCounter;      // Count remaining seats
+
+    document.getElementById("bookings").innerText = seatCounter;        // Update Booked seats
+
+    let remainingSeats = document.getElementById("remainings");
+    remainingSeats.innerText = remainings;      // Update remainings seats
+
     document.getElementById("text").innerText = "Total Seat Selected: " + seatCounter;
 };
 
 // Cancel Seat
 function cancelSeat() {
     seatCounter -= 1;
+    remainings = 60 - seatCounter;      // Count remaining seats
+
+    document.getElementById("bookings").innerText = seatCounter;        // Update Booked seats
+
+    let remainingSeats = document.getElementById("remainings");
+    remainingSeats.innerText = remainings;      // Update remainings seats
+
     document.getElementById("text").innerText = "Total Seat Selected: " + seatCounter;
 };
 
 // Select Seat
 function seatSelect(seatNo) {
     let seatCheckbox = document.getElementById(seatNo);     // Select the seat from seat no.
-    
+
     // If Seat is checked
     if (seatCheckbox.checked == true) {
         console.log(`Seat Added: ${seatNo}`);
